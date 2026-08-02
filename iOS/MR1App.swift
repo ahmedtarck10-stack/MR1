d515d70de04925174cc62fb36cd2121dad9a2d41
ahmedtarck10-stack <ahmedtarck10@icloud.com>
2026-08-02 13:43:53 +0300

    Add iOS App entry point with SwiftUI

---
+++ iOS/MR1App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MR1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
